93843e9d5c4f4c68c98fc1c9d437380b1d0b2796
Harsh selarka <harshselarka2@gmail.com>
2025-01-08 16:09:16 +0530

    loops

--- index.php
+++ index.php
@@ -3,8 +3,29 @@
 <head>
   
     <title>Document-commit 1,2</title>
+    <style>
+        body{
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family:sans-sarif;
+        }
+    </style>
 </head>
 <body>
-    <h1>hello world123</h1>
+     <?php
+       $books = ["atomic habits ",
+       "prena nu zarnu",
+        " 7 habits of highly influnteal ppl"];
+     ?>
+    <h1>you have read <?=$books[2]?></h1>
+     <ul>
+        <?php
+         foreach($books as $book){
+            echo "you have read $book". "<br>";
+        }
+        ?>
+     </ul>
 </body>
 </html>
